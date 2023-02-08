8c04b46d301e83fd0c8fc3ce3e6d53af5494d101
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 11:28:28 +0530

    Add my first react component

---
+++ dabbleWithVite/src/Basics/Basics.tsx
@@ -0,0 +1,11 @@
+import "./Basics.css";
+
+const Basics = function learnBasicsOfReact() {
+  return (
+    <div className="mainDiv">
+      <p className="doggyLog">A dog is logged!</p>
+    </div>
+  );
+};
+
+export default Basics;
