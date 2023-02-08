c46f2736ccefec2471ad71f1a9eaeabf7670d768
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-08 14:39:24 +0530

    Export components instead of constants

--- dabbleWithVite/src/Basics/Basics.tsx
+++ dabbleWithVite/src/Basics/Basics.tsx
@@ -1,15 +1,15 @@
 import Button from "../Components/Button/Button";
 import "./Basics.css";
 
-const Basics = function learnBasicsOfReact() {
+function Basics() {
   return (
     <div className="mainDiv">
       <p className="doggyLog">A dog is logged!</p>
     </div>
   );
-};
+}
 
-const NestedList = function () {
+function NestedList() {
   return (
     <div>
       <ul>
@@ -19,9 +19,9 @@ const NestedList = function () {
       <Button />
     </div>
   );
-};
+}
 
-const TestingJSX = function () {
+function TestingJSX() {
   const button = (
     <div>
       <h1>This is a button!</h1>
@@ -30,7 +30,22 @@ const TestingJSX = function () {
   );
   console.log(button);
   return button;
-};
+}
+
+function JSXNavBar() {
+  const navBar = (
+    <nav>
+      <h1>DogeShack</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+
+  return navBar;
+}
 
 export default Basics;
-export { NestedList, TestingJSX };
+export { NestedList, TestingJSX, JSXNavBar };
