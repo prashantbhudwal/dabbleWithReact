55ed82ccdc93c499fcee6eeeac6bfe6e832435c0
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-22 16:30:17 +0530

    Yet another restructuring

--- src/Template/Components/Component/index.tsx
+++
@@ -1,6 +0,0 @@
-import "./index.css";
-function TemplateComponent() {
-  return <p>Template</p>;
-}
-
-export default TemplateComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./Features/AutoRenderingArrays/App";
-import App from "./dabble/Features/PracticingProps/App";
-// import "./main.css";
+// import App from "./Current/App";
+import App from "./CurrentApp/App";
+import "./reset.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
