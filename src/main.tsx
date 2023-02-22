8c1968d4469c2e33bc4d45e066fb9b11cd8835ec
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-22 17:05:16 +0530

    Add a styled component to the app template

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./Current/App";
-import App from "./libarary/Features/CounterWithState/App";
+import App from "./Current/App";
+
 import "./reset.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
