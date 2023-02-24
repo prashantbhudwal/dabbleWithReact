bbb80d551334ffa52b9832fe7447fd861b87997a
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-24 15:35:24 +0530

    LO: Pure components

---
+++ src/learningReact/PureComponent/Clock.tsx
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+const StyledClock = styled.div`
+  .day {
+    background: #2ff;
+    color: #222;
+  }
+  .night {
+    background: #202;
+    color: #fff;
+  }
+`;
+
+export default function Clock({ time }) {
+  let hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = "night";
+  } else {
+    className = "day";
+  }
+  return (
+    <StyledClock>
+      <h1 className={className}>{time.toLocaleTimeString()}</h1>
+    </StyledClock>
+  );
+}
