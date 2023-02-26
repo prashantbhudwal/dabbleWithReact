078d93c6dc4f8ff55df0310c3773a6a1b44da0e4
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-26 07:56:45 +0530

    Take a detour to a different example

--- src/Current/App.tsx
+++ src/Current/App.tsx
@@ -1,23 +1,8 @@
 import styled from "@emotion/styled";
-import Button from "./Button";
-import Header from "./Header";
-import "./styles.css";
-import ThemeContext from "./themeContext";
 
 const StyledApp = styled.div``;
 
 function App() {
-  const switchTheme = function () {
-    console.log("Switched");
-  };
-
-  return (
-    <ThemeContext.Provider value={"light"}>
-      <StyledApp>
-        <Header />
-        <Button handleClick={switchTheme} />
-      </StyledApp>
-    </ThemeContext.Provider>
-  );
+  return <StyledApp>App</StyledApp>;
 }
 export default App;
