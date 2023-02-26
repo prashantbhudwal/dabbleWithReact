bcf481aa2f5b61a0af56102f066dcbf49a40b983
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-26 08:10:05 +0530

    LO: useContext from a component

--- src/Current/App.tsx
+++ src/Current/App.tsx
@@ -1,8 +1,26 @@
-import styled from "@emotion/styled";
+import Heading from "./Heading.js";
+import Section from "./Section.js";
+import "./styles.css";
 
-const StyledApp = styled.div``;
-
-function App() {
-  return <StyledApp>App</StyledApp>;
+export default function Page() {
+  return (
+    <Section>
+      <Heading>Title</Heading>
+      <Section>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
 }
-export default App;
