1e2feb5a172df7d98bb0dfe5d3d94ddee8b4c7a7
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-02-26 09:06:35 +0530

    LO: Providing context with Context.Provider

--- src/Current/Heading.tsx
+++ src/Current/Heading.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
 import { LevelContext } from "./LevelContext";
+import { useContext } from "react";
 
 export default function Heading({ children }) {
   const level = useContext(LevelContext);
--- src/Current/Section.tsx
+++ src/Current/Section.tsx
@@ -1,3 +1,12 @@
+import { LevelContext } from "./LevelContext";
+import { useContext } from "react";
 export default function Section({ children }) {
-  return <section className="section">{children}</section>;
+    // Consumes the context provided by default value in the create context 
+     const level = useContext(LevelContext);
+  return (
+    <section className="section">
+    {/* Provides new context to the children components */}
+      <LevelContext.Provider value={level + 1}>{children}</LevelContext.Provider>
+    </section>
+  );
 }
