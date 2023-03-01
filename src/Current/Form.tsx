4e5c56a40cc3933993a03739652962e02bc18ef2
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-01 17:39:01 +0530

    LO:  Handling radio buttons in react forms

--- src/Current/Form.tsx
+++ src/Current/Form.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
 
 const StyledForm = styled.form`
   width: 500px;
@@ -8,6 +9,20 @@ const StyledForm = styled.form`
   flex-direction: column;
   align-items: flex-start;
   gap: 1em;
+
+  .radioContainer {
+    display: flex;
+    gap: 1em;
+  }
+
+  .ratingRadio {
+    width: 1.5em;
+    height: 1.5em;
+  }
+
+  .ratingRadio:hover {
+    background-color: blue;
+  }
 `;
 
 const Input = styled.input`
@@ -20,11 +35,64 @@ const Input = styled.input`
 `;
 
 export default function Form() {
+  const [formState, setFormState] = useState({
+    fullName: "",
+    rating: "0",
+    isRated: true,
+  });
+
+  console.log(formState.fullName, formState.rating, formState.isRated);
+
+  const handleRatingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, checked, type } = e.target;
+    setFormState((prevState) => ({
+      ...prevState,
+      [name]: type === "checkbox" ? checked : value,
+      //Doing this will give and error [name]: type === "checkbox" ? [checked] : [value],
+    }));
+  };
+
   return (
     <StyledForm>
-      <label>
-        <Input type={"text"} placeholder="Name" />
+      <Input
+        type={"text"}
+        placeholder="Full Name"
+        name="fullName"
+        onChange={handleRatingChange}
+        value={formState.fullName}
+      />
+      <label className="radioContainer">
+        Your Ratings
+        <br />
+        <Input
+          type={"radio"}
+          name="rating"
+          id="rating__one"
+          value={1}
+          className="ratingRadio"
+          onChange={handleRatingChange}
+          checked={formState.rating === "1"}
+        ></Input>
+        <Input
+          type={"radio"}
+          name="rating"
+          id="rating__two"
+          value={2}
+          className="ratingRadio"
+          onChange={handleRatingChange}
+          checked={formState.rating === "2"}
+        ></Input>
+        <Input
+          type={"radio"}
+          name="rating"
+          id="rating__three"
+          value={3}
+          className="ratingRadio"
+          onChange={handleRatingChange}
+          checked={formState.rating === "3"}
+        ></Input>
       </label>
+      <button type="submit">Submit Rating</button>
     </StyledForm>
   );
 }
