2609029b8847a67329fe5c51394bb303cb250cbe
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-01 18:23:15 +0530

    🧹 New Current Form

--- src/Current/Form.tsx
+++ src/Current/Form.tsx
@@ -1,116 +1,30 @@
 import styled from "@emotion/styled";
-import { useState } from "react";
 
-const StyledApp = styled.div`
-  .form-container {
-    height: 100vh;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+const StyledForm = styled.form`
+  width: 500px;
+  height: 700px;
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1em;
+`;
+
+const Input = styled.input`
+  &::placeholder {
   }
-
-  .form {
-    background-color: white;
-    padding: 50px;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    box-shadow: 5px 5px 15px -1px rgba(0, 0, 0, 0.75);
+  &:placeholder-shown {
   }
-
-  .form--input {
-    width: 100%;
-    margin-bottom: 20px;
-    height: 40px;
-    border-radius: 5px;
-    border: 1px solid gray;
-    padding: 5px;
-    font-family: "Inter", sans-serif;
-  }
-
-  .form--submit {
-    width: 50%;
-    padding: 10px;
-    border-radius: 5px;
-    color: white;
-    background-color: #639;
-    border: none;
-    cursor: pointer;
-  }
-
-  .form--marketing {
-    display: flex;
-    margin-bottom: 20px;
-    align-items: center;
-  }
-
-  .form--marketing > input {
-    margin-right: 10px;
+  &:focus {
   }
 `;
 
 export default function Form() {
-  const [formState, setFormState] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-    okayToEmail: false,
-  });
-
-  const handleChange = (event: any) => {
-    const { name, value, checked, type } = event.target;
-    setFormState((prevState) => ({
-      ...prevState,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-  };
-
-  function handleSubmit(event: any) {
-    event.preventDefault();
-    console.log(formState);
-  }
-
   return (
-    <StyledApp>
-      <div className="form-container">
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email address"
-            className="form--input"
-            name="email"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            className="form--input"
-            name="password"
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            placeholder="Confirm password"
-            className="form--input"
-            name="confirmPassword"
-            onChange={handleChange}
-          />
-
-          <div className="form--marketing">
-            <input
-              id="okayToEmail"
-              type="checkbox"
-              name="okayToEmail"
-              onChange={handleChange}
-            />
-            <label htmlFor="okayToEmail">I want to join the newsletter</label>
-          </div>
-          <button className="form--submit">Sign up</button>
-        </form>
-      </div>
-    </StyledApp>
+    <StyledForm>
+      <label>
+        <Input type={"text"} placeholder="Name" />
+      </label>
+    </StyledForm>
   );
 }
