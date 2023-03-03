7d5c1567044f38ca9ede9889d9a9fe54029936f1
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2023-03-03 08:12:22 +0530

    New App

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 //
 // import App from "./Current/App";
 // import App from "./Current/Form";
-import App from "./Current/Form";
+import App from "./Current/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
